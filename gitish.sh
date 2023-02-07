eab0aa8390ac5d66cc52e627f146876d14e80bc2
pj8912 <jpintofun2b@gmail.com>
2023-02-07 09:54:09 +0530

    , intial commit

---
+++ gitish.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+echo "this automates git add, commit, push in a single line"
+echo "Enter add type , commit message" 
+read addp message
+
+
+
+git add "$addp"
+git commit -m "$message"
+git push
+
