b520486126cb3e724309614d114957954f088ba5
Aadv1k <aadv1k@outlook.com>
2023-02-16 20:24:09 +0530

    Fix the impl; add windows

--- gitish.bat
+++ gitish.bat
@@ -1,3 +1,3 @@
-git add --all
+git add .
 git commit -m "%*"
 git push
--- gitish.sh
+++ gitish.sh
@@ -5,6 +5,8 @@ echo " / __|(_)| |_  ___| |_  "
 echo "| (_ || ||  _|(_-/|   \ "
 echo " \___||_| \__|/__/|_||_|"
 
+all=$@
+
 git add .
-git commit -m "$@"
+git commit -m "$all"
 git push
