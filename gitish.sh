7df7491121ea6a7b3a2e2ff80c4955bdbbede1fb
Aadv1k <aadv1k@outlook.com>
2023-02-16 20:07:17 +0530

    hello

--- gitish.sh
+++ gitish.sh
@@ -4,9 +4,6 @@ echo "this automates git add, commit, push in a single line"
 echo "Enter add type , commit message" 
 read addp message
 
-
-
 git add "$addp"
 git commit -m "$message"
-git push
-
+#git push
