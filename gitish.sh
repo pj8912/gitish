baa4a0a5960e1003eea08d3de44e6357e9aef703
Aadv1k <aadv1k@outlook.com>
2023-02-16 20:34:58 +0530

    fix the code

--- gitish.sh
+++ gitish.sh
@@ -5,8 +5,16 @@ echo " / __|(_)| |_  ___| |_  "
 echo "| (_ || ||  _|(_-/|   \ "
 echo " \___||_| \__|/__/|_||_|"
 
-all=$@
 
-git add .
-git commit -m "$all"
+usage() {
+  echo "$0 <ADD> <MSG>"
+  exit
+}
+
+if [ $# -lt 2 ]; then
+  usage
+fi
+
+git add $1
+git commit -m "$2"
 git push
